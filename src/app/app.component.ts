f7e9d46eb4662e277ae37d5bdf874b983a162e28
andy <weat0212@gmail.com>
2022-03-02 16:22:57 +0800

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+import { Subject } from 'rxjs';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  test = 'Hello';
+
+  var source = Rx.Observable.interval(1000).take(3);
+
+  var observerA = {
+    next: value => console.log('A next: ' + value),
+    error: error => console.log('A error: ' + error),
+    complete: () => console.log('A complete!')
+  }
+
+  var observerB = {
+    next: value => console.log('B next: ' + value),
+    error: error => console.log('B error: ' + error),
+    complete: () => console.log('B complete!')
+  }
+
+  source.subscribe(observerA);
+  source.subscribe(observerB);
+}
